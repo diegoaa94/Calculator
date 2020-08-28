83518cb068bcc39d72526ef28c34f5d2a1753fa8
Diego Angulo <diegoaa0294@gmail.com>
2020-08-28 14:01:12 -0600

    Created Calculator project

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class Calculator
+{
+    public int Add(int a, int b) { return a + b; }
+}
+
+public class Logger
+{
+    public void Log(string message) { Console.WriteLine(message); }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Logger logger = new Logger();
+        Calculator calculator = new Calculator();
+        int firstNumber;
+        int secondNumber;
+        int result;
+
+        logger.Log("Enter first number: ");
+        firstNumber = Convert.ToInt32(Console.ReadLine());
+            
+        logger.Log("Enter second number: ");
+        secondNumber = Convert.ToInt32(Console.ReadLine());
+            
+        result = calculator.Add(firstNumber, secondNumber);
+
+        logger.Log("Result:\n" + firstNumber + " + " + secondNumber + " = " + result);
+    }
+}
